7faf7b034cab1d513bc98176781b4e59bdba9f4c
chunklele <chunklele@users.noreply.github.com>
2016-06-06 14:40:33 -0400

    Added grid sample to game.

--- build/shipio.js
+++ build/shipio.js
@@ -72,10 +72,10 @@ var shipio = (function (stella) {
     this.stella = stella;
     this.input = this.stella.input;
 
+    this.grid = new grid({ width: 300, height: 300, blockSize: 45 });
     this.localPlayer = new player(this, { x: 100, y: 100, width: 100, height: 75 });
     this.localPlayer.setSpeed({ forwardSpeed: 0.023, topForwardSpeed: 3, backwardSpeed: 0.01, rotateSpeed: 0.05 })
 });
-
 
 shipio.prototype.update = function () {
     this.localPlayer.update();
@@ -83,6 +83,7 @@ shipio.prototype.update = function () {
 
 shipio.prototype.draw = function (ctx) {
     ctx.fillStyle = "white";
+    this.grid.draw(ctx);
     this.localPlayer.draw(ctx);
 
     if (DEBUG_MODE) {
